585964a7e2fe9b581556224215699840aa68465b
kisa <programming.ksy.2@gmail.com>
2024-06-25 23:46:50 +0900

    style : Body0 formatted
    Body0 formatted

--- components/body0.py
+++ components/body0.py
@@ -38,39 +38,69 @@ class AutoBuildBody(UserControl):
         self.page = page
         self.jenkins = jenkins
 
-        self.text_job = Text("Job設定", color="#f8f8ff")
-        self.text_job_name = TextField(label="Job名", text_size=20, border_width=3, width=384, border_radius=10)
-        self.text_job_desc = TextField(label="Job概要", text_size=20, border_width=3, width=384, border_radius=10)
+        # Job関連のウィジェット作成
+        self.text_job = Text("Job設定", size=17, color="#f8f8ff")
+        self.text_job_name = TextField(label="Job名(必須)",
+                                       text_size=15,
+                                       border_width=3,
+                                       width=384,
+                                       border_radius=10,
+                                       hint_text="プロジェクト名と同じであること")
+        self.text_job_desc = TextField(label="Job概要",
+                                       text_size=15,
+                                       border_width=3,
+                                       width=384,
+                                       border_radius=10,
+                                       hint_text="例) M222のAutoビルド用")
 
-        self.text_git = Text("Git設定", color="#f8f8ff")
-        self.text_git_url = TextField(label="Git URL", text_size=20, border_width=3, border_radius=10)
-        self.text_git_branch = TextField(label="ブランチ名", text_size=20, border_width=3, width=384, border_radius=10)
-        self.text_build_folder = TextField(label="ビルド対象フォルダ", text_size=20, border_width=3, width=384, border_radius=10)
-        self.text_teams_url = TextField(label="Teams URL", text_size=20, border_width=3, border_radius=10, password=True, can_reveal_password=True)
+        # Git関連のウィジェット作成
+        self.text_git = Text("Git設定", size=17, color="#f8f8ff")
+        self.text_git_url = TextField(label="Git URL(必須)",
+                                      text_size=15,
+                                      border_width=3,
+                                      border_radius=10)
+        self.text_git_branch = TextField(label="ブランチ名",
+                                         text_size=15,
+                                         border_width=3,
+                                         width=384,
+                                         border_radius=10)
+        self.text_build_folder = TextField(label="ビルド対象フォルダ",
+                                           text_size=15,
+                                           border_width=3,
+                                           width=384,
+                                           border_radius=10)
 
-        self.text_teams = Text("Teams設定", color="#f8f8ff")
+        # Teams関連のウィジェット作成
+        self.text_teams = Text("Teams設定", size=17, color="#f8f8ff")
+        self.text_teams_url = TextField(label="Teams URL(必須)",
+                                        text_size=15,
+                                        border_width=3,
+                                        border_radius=10,
+                                        password=True,
+                                        can_reveal_password=True)
 
-        self.text_buid_setting = Text("Build設定", color="#f8f8ff")
-        self.text_buid_data = Text("ビルド実行曜日", size=15, color="#f8f8ff", opacity=1, animate_opacity=200)
+        # Build関連のウィジェット作成
+        self.text_buid_setting = Text("Build設定", size=17, color="#f8f8ff")
+        self.text_buid_data = Text("ビルド実行曜日", size=15, color="#f8f8ff", opacity=1)
+        self.chkbox_sun = Checkbox(label="Sun", shape=RoundedRectangleBorder(radius=5), value=False, scale=1.3)
+        self.chkbox_mon = Checkbox(label="Mon", shape=RoundedRectangleBorder(radius=5), value=True, scale=1.3)
+        self.chkbox_tue = Checkbox(label="Tue", shape=RoundedRectangleBorder(radius=5), value=True, scale=1.3)
+        self.chkbox_wed = Checkbox(label="Wed", shape=RoundedRectangleBorder(radius=5), value=True, scale=1.3)
+        self.chkbox_thu = Checkbox(label="Thu", shape=RoundedRectangleBorder(radius=5), value=True, scale=1.3)
+        self.chkbox_fri = Checkbox(label="Fri", shape=RoundedRectangleBorder(radius=5), value=True, scale=1.3)
+        self.chkbox_sat = Checkbox(label="Sat", shape=RoundedRectangleBorder(radius=5), value=False, scale=1.3)
 
-        self.chkbox_sun = Checkbox(label="Sun", shape=RoundedRectangleBorder(radius=10), value=False, scale=1.3)
-        self.chkbox_mon = Checkbox(label="Mon", shape=RoundedRectangleBorder(radius=10), value=False, scale=1.3)
-        self.chkbox_tue = Checkbox(label="Tue", shape=RoundedRectangleBorder(radius=10), value=False, scale=1.3)
-        self.chkbox_wed = Checkbox(label="Wed", shape=RoundedRectangleBorder(radius=10), value=False, scale=1.3)
-        self.chkbox_thu = Checkbox(label="Thu", shape=RoundedRectangleBorder(radius=10), value=False, scale=1.3)
-        self.chkbox_fri = Checkbox(label="Fri", shape=RoundedRectangleBorder(radius=10), value=False, scale=1.3)
-        self.chkbox_sat = Checkbox(label="Sat", shape=RoundedRectangleBorder(radius=10), value=False, scale=1.3)
-
+        # 実行ボタンのウィジェット作成
         self.dlg = AlertDialog(
+            # ダイアログの外側をクリックして閉じれないようにする
             modal=True,
             title=Text("Please confilm"),
-            content=Text("入力されていない箇所があります"),
+            content=Text("必須項目をしてください"),
             actions=[
-                TextButton("閉じる", on_click=self.handle_click_dlg),
+                TextButton("閉じる", on_click=self.handle_close_dlg),
             ],
             actions_alignment=MainAxisAlignment.END,
         )
-
         self.button = ElevatedButton(
             text="実行",
             bgcolor="#0054FF",
@@ -78,6 +108,7 @@ class AutoBuildBody(UserControl):
             on_click=self.handle_open_dlg
         )
 
+        # タイムピッカーのウィジェット作成
         self.timer_picker = CupertinoTimerPicker(
             value=3600,
             second_interval=10,
@@ -85,7 +116,7 @@ class AutoBuildBody(UserControl):
             mode=CupertinoTimerPickerMode.HOUR_MINUTE,
             on_change=self.handle_timer_picker_change,
         )
-
+        # タイムピッカーの値はRefで参照する
         self.timer_picker_value_ref = Ref[Text]()
 
     def build(self):
@@ -93,63 +124,68 @@ class AutoBuildBody(UserControl):
             alignment=MainAxisAlignment.START,
             horizontal_alignment=CrossAxisAlignment.START,
             controls=[
-            self.text_job,
+                self.text_job,
                 Row(
                     controls=[
                         self.text_job_name,
                         self.text_job_desc
-                    ]),
-            Divider(height=5, color="white12", thickness=2),
-            self.text_git,
-            self.text_git_url,
+                    ]
+                ),
+                Divider(height=5, color="white12", thickness=2),
+                self.text_git,
+                self.text_git_url,
                 Row(
                     controls=[
                         self.text_git_branch,
                         self.text_build_folder
-                    ]),
-            Divider(height=5, color="white12", thickness=2),
-            self.text_teams,
-            self.text_teams_url,
-            Divider(height=5, color="white12", thickness=2),
-            self.text_buid_setting,
-            Row(
-                spacing=35,
-                controls=[
-                self.text_buid_data,
-                self.chkbox_sun,
-                self.chkbox_mon,
-                self.chkbox_tue,
-                self.chkbox_wed,
-                self.chkbox_thu,
-                self.chkbox_fri,
-                self.chkbox_sat
-                ]),
-            Row(
-                tight=True,
-                spacing=25,
-                controls=[
-                    Text("ビルド開始時間", size=15, color="f8f8ff"),
-                    TextButton(
-                        content=Text("00:00",
-                                    size=20,
-                                    color="white",
-                                    ref=self.timer_picker_value_ref
-                                ),
-                        style=ButtonStyle(color=colors.RED),
-                        on_click=lambda _: self.page.show_bottom_sheet(
-                            CupertinoBottomSheet(
-                                self.timer_picker,
-                                height=216,
-                                padding=padding.only(top=6),
-                            )
+                    ]
+                ),
+                Divider(height=5, color="white12", thickness=2),
+                self.text_teams,
+                self.text_teams_url,
+                Divider(height=5, color="white12", thickness=2),
+                self.text_buid_setting,
+                Row(
+                    spacing=35,
+                    controls=[
+                        self.text_buid_data,
+                        self.chkbox_sun,
+                        self.chkbox_mon,
+                        self.chkbox_tue,
+                        self.chkbox_wed,
+                        self.chkbox_thu,
+                        self.chkbox_fri,
+                        self.chkbox_sat
+                    ]
+                ),
+                Row(
+                    tight=True,
+                    spacing=25,
+                    controls=[
+                        Text("ビルド開始時間", size=15, color="f8f8ff"),
+                        TextButton(
+                            content=Text("22:00",
+                                        size=20,
+                                        color="white",
+                                        ref=self.timer_picker_value_ref
+                                    ),
+                            style=ButtonStyle(color=colors.RED),
+                            on_click=lambda _: self.page.show_bottom_sheet(
+                                CupertinoBottomSheet(
+                                    self.timer_picker,
+                                    height=216,
+                                    padding=padding.only(top=6),
+                                )
+                            ),
                         ),
-                    ),
-                ]),
-            Row(
-                alignment=MainAxisAlignment.END,
-                controls=[
-                self.button,
-                ]),
+                    ]
+                ),
+                Row(
+                    alignment=MainAxisAlignment.END,
+                    controls=[
+                        self.button
+                    ]
+                ),
             ]
         )
 
@@ -158,7 +194,23 @@ class AutoBuildBody(UserControl):
         self.timer_picker_value_ref.current.value = time.strftime("%H : %M", time.gmtime(val))
         self.page.controls[0].controls[0].content.controls[1].content.update()
 
-    def click_button(self, e):
+    def handle_close_dlg(self, e):
+        self.dlg.open = False
+        e.control.page.update()
+
+    def handle_open_dlg(self, e):
+        result = self.check_input(e)
+
+        # インプット確認で問題があればダイアログを表示し、
+        # 問題がなければJenkinsへデータを送信する
+        if result != 0:
+            e.control.page.dialog = self.dlg
+            self.dlg.open = True
+            e.control.page.update()
+        else:
+            self.send_input(e)
+
+    def send_input(self, e):
         # テキストのデータ取得
         job_name = e.page.controls[0].controls[0].content.controls[1].content.text_job_name.value
         job_desc = e.page.controls[0].controls[0].content.controls[1].content.text_job_desc.value
@@ -208,16 +260,3 @@ class AutoBuildBody(UserControl):
             return (result := -1)
 
         return result
-
-    def handle_click_dlg(self, e):
-        self.dlg.open = False
-        e.control.page.update()
-
-    def handle_open_dlg(self, e):
-        result = self.check_input(e)
-        if result != 0:
-            e.control.page.dialog = self.dlg
-            self.dlg.open = True
-            e.control.page.update()
-        else:
-            self.click_button(e)
